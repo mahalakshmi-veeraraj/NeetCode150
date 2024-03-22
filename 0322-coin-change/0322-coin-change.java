7b1cc89fb11e307f8adda5aeccb293d57102928f
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-03-22 09:34:16 +0530

    Time: 25 ms (34.50%), Space: 45.1 MB (23.34%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,33 +1,24 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         if (amount <= 0) return amount;
-        if (coins.length == 1){
-            if (coins[0] > amount) return -1;
-            else {
-                if (amount % coins[0] != 0) return -1;
-                else return amount / coins[0];
-            }
-        }
-        
         
         int[] dp = new int[amount + 1];
         for (int i = 0; i < dp.length; i++) {
             dp[i] = -1;
         }
-        dp[0] = 0;
-        minCoinsNeeded(coins, dp, amount);
-        if (dp[amount] == 1000000000) return -1;
-        return dp[amount];
+        return find(dp, coins, amount) == 1000000000 ? -1 : dp[amount];   
     }
-    private int minCoinsNeeded(int[] coins, int[] dp, int amount) {
+    
+    private int find (int[] dp, int[] coins, int amount) {
+        if (amount <= 0) return 0;
         if (dp[amount] != -1) return dp[amount];
         
-        int minCoinsNeeded = 1000000000;
+        int minNumberOfCoinsNeededToMakeAmount = 1000000000;
         for (int i = 0; i < coins.length; i++) {
             if (coins[i] > amount) continue;
-            minCoinsNeeded = Math.min(minCoinsNeeded, minCoinsNeeded(coins, dp, amount - coins[i]) + 1);
+            minNumberOfCoinsNeededToMakeAmount = Math.min(minNumberOfCoinsNeededToMakeAmount, find(dp, coins, amount - coins[i]) + 1);    
         }
-        dp[amount] = minCoinsNeeded;
+        dp[amount] = minNumberOfCoinsNeededToMakeAmount;
         return dp[amount];
     }
 }
