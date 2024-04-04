d8df9801f31712e93f055832505caac207673bf3
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-04-04 17:33:25 +0530

    Time: 80 ms (26.52%), Space: 55.3 MB (99.96%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,50 +1,20 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
-        int[] answer = new int[n];
         int[] nge = new int[n];
         Stack<Integer> stack = new Stack<>();
-        stack.add(n - 1);
+        stack.push(n - 1);
         for (int i = n - 2; i >= 0; i--) {
-            while (!stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i]) {
+            while (!stack.empty() && temperatures[stack.peek()] <= temperatures[i]) {
                 stack.pop();
             }
-            
-            nge[i] = stack.isEmpty() ? 0 : stack.peek();
-            stack.add(i);
+            nge[i] = stack.empty() ? 0 : stack.peek();
+            stack.push(i);
         }
+        int[] result = new int[n];
         for (int i = 0; i < nge.length; i++) {
-            if (nge[i] == 0) continue;
-            answer[i] = nge[i] - i;
+            result[i] = nge[i] == 0 ? 0 : nge[i] - i;
         }
-        return answer;
-    }
-    public int[] bruteForce (int[] temperatures) { // Got Time Limit Exceeded Error by this Brute Force approach.
-        int n = temperatures.length;
-        int[] answer = new int[n];
-        int numberOfDays = 0;
-        boolean isFound = false;
-        for (int i = 0; i < n; i++) {
-            numberOfDays = 0;
-            isFound = false;
-            for (int j = i + 1; j < n; j++) {
-                numberOfDays++;
-                if (temperatures[j] > temperatures[i]) {
-                    isFound = true;
-                    break;
-                }
-            }
-            if (!isFound) numberOfDays = 0;
-            answer[i] = numberOfDays;
-        }
-        return answer;
+        return result;
     }
 }
-
-
-//   0  1  2  3  4  5  6  7
-// [73,74,75,71,69,72,76,73]
-// [74,75,76,72,72,76,0,0]
-// [0, 1, 2, 3, 4, 5, 6, 7]
-// [1, 2, 6, 5, 5, 6, 0, 0]
-// [1, 1, 4, 2, 1, 1, 0, 0]
