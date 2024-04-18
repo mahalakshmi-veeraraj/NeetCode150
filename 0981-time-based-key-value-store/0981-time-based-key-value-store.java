4f1f9546cecc64d0479834015488b93ede8118fd
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-04-18 12:07:58 -0600

    Time: 129 ms (94.33%), Space: 117.9 MB (95.98%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -1,17 +1,22 @@
 class TimeMap {
     Map<String, List<Pair>> hashMap = new HashMap<>(); 
     class Pair {
-        int timestamp;
-        String value;
+        private int timestamp;
+        private String value;
         public Pair(int timestamp, String value) {
             this.timestamp = timestamp;
             this.value = value;
+        }
+        public int getTimestamp() {
+            return this.timestamp;
+        }
+        public String getValue() {
+            return this.value;
         }
     }
     public TimeMap() {
         
     }
-    
     public void set(String key, String value, int timestamp) {
         List<Pair> listPair;
         if (!hashMap.containsKey(key)) {
@@ -26,11 +31,10 @@ class TimeMap {
     
     public String get(String key, int timestamp) {
         if (!hashMap.containsKey(key)) return "";
-        
         List<Pair> listPair = hashMap.get(key);
         for (int i = listPair.size() - 1; i >= 0; i--) {
-            if (listPair.get(i).timestamp <= timestamp) {
-                return listPair.get(i).value;
+            if (listPair.get(i).getTimestamp() <= timestamp) {
+                return listPair.get(i).getValue();
             }
         }
         return "";
