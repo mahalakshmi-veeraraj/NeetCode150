baca36bbcf3b910ed6dbc222c889746cdd96ce26
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-04-19 06:25:08 -0600

    Time: 1 ms (100.00%), Space: 46.2 MB (18.88%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,38 +1,39 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int totalElements = nums1.length + nums2.length;
-        int median1Index = totalElements / 2;
-        int median2Index = totalElements % 2 != 0 ? median1Index : median1Index - 1;
-        int median1 = 0;
-        int median2 = 0;
-        int n = 0;
-        int m = 0;
-        int index = 0;
-        while (n < nums1.length && m < nums2.length) {
-            if (nums1[n] <= nums2[m]) {
-                if (index == median1Index) median1 = nums1[n];
-                if (index == median2Index) median2 = nums1[n];
-                n++;
-            }
+        int n = nums1.length;
+        int m = nums2.length;
+        if (n > m) return findMedianSortedArrays(nums2, nums1);
+        int totalElements = n + m;
+        int totalElementsOnLeft = (totalElements + 1) / 2;
+        int low = 0;
+        int high = n;
+        int middle1 = 0;
+        int middle2 = 0;
+        int l1 = 0;
+        int l2 = 0;
+        int r1 = 0;
+        int r2 = 0;
+        while (low <= high) {
+            middle1 = (low + high) / 2;
+            middle2 = totalElementsOnLeft - middle1;
+            
+            if (middle1 - 1 >= 0) l1 = nums1[middle1 - 1];
+            else l1 = Integer.MIN_VALUE;
+            if (middle1 < n) r1 = nums1[middle1];
+            else r1 = Integer.MAX_VALUE;
+            
+            if (middle2 - 1 >= 0) l2 = nums2[middle2 - 1];
+            else l2 = Integer.MIN_VALUE;
+            if (middle2 < m) r2 = nums2[middle2];
+            else r2 = Integer.MAX_VALUE;
+            
+            if (l2 > r1) low = middle1 + 1;
+            else if (l1 > r2) high = middle1 - 1;
             else {
-                if (index == median1Index) median1 = nums2[m];
-                if (index == median2Index) median2 = nums2[m];
-                m++;
+                if (totalElements % 2 != 0) return Math.max(l1, l2);
+                else return ((double)Math.max(l1, l2) + (double)Math.min(r1, r2)) / 2;
             }
-            index++;
         }
-        while (n < nums1.length) {
-            if (index == median1Index) median1 = nums1[n];
-            if (index == median2Index) median2 = nums1[n];
-            n++;
-            index++;
-        }
-        while (m < nums2.length) {
-            if (index == median1Index) median1 = nums2[m];
-            if (index == median2Index) median2 = nums2[m];
-            m++;
-            index++;
-        }
-        return ((double) median1 + (double) median2) / 2;
+        return 0;
     }
 }
