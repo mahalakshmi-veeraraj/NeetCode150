589f7ec84962c17d53952ef0aca3c2f4904cfed0
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-05-23 07:48:44 +0530

    Time: 8 ms (5.61%), Space: 45.3 MB (5.08%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -20,17 +20,14 @@ class Solution {
         
         preOrderTraversal(root, rootStrBuilder);
         preOrderTraversal(subRoot, subRootStrBuilder);
-        System.out.println(rootStrBuilder.toString());
-        System.out.println(subRootStrBuilder.toString());
         return rootStrBuilder.toString().indexOf(subRootStrBuilder.toString()) != -1;
     }
     private void preOrderTraversal(TreeNode root, StringBuilder strBuilder) {
         if (root == null) {
-            strBuilder.append("null");
+            strBuilder.append("#");
             return;
         }
-        strBuilder.append("R");
-        strBuilder.append(root.val);
+        strBuilder.append("["+root.val+"]");
         preOrderTraversal(root.left, strBuilder);
         preOrderTraversal(root.right, strBuilder);
     }
