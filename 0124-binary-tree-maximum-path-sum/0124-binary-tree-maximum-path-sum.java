aef7cb0764b2adcf933aa133369c4944c409eba4
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-06-29 14:41:48 +0530

    Time: 1 ms (16.28%), Space: 44 MB (68.02%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -15,22 +15,22 @@
  */
 class Solution {
     class MutableInteger {
-        public int value;
+        int value;
         public MutableInteger(int value) {
             this.value = value;
         }
     }
     public int maxPathSum(TreeNode root) {
-        MutableInteger mutableInteger = new MutableInteger(Integer.MIN_VALUE);
-        sum (root, mutableInteger);
-        return mutableInteger.value;
+        MutableInteger answer = new MutableInteger(Integer.MIN_VALUE);
+        findMaxPathSum(root, answer);
+        return answer.value;
     }
-    private int sum (TreeNode root, MutableInteger mutableInteger) {
+    private int findMaxPathSum (TreeNode root, MutableInteger answer) {
         if (root == null) return 0;
         
-        int lst = Math.max(sum(root.left, mutableInteger), 0);
-        int rst = Math.max(sum(root.right, mutableInteger), 0);
-        mutableInteger.value = Math.max(mutableInteger.value, root.val + lst + rst);
-        return Math.max(lst , rst) + root.val;
+        int maxLeftSubTreePathSum = Math.max(0, findMaxPathSum (root.left, answer));
+        int maxRightSubTreePathSum = Math.max(0, findMaxPathSum (root.right, answer));
+        answer.value = Math.max(answer.value, root.val + maxLeftSubTreePathSum + maxRightSubTreePathSum);
+        return Math.max(maxLeftSubTreePathSum, maxRightSubTreePathSum) + root.val;
     }
 }
