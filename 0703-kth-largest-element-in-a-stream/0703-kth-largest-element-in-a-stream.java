458db74eeed6ccfb447de4c40bd35f706a5a15c7
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-07-01 20:34:26 +0530

    Time: 11 ms (99.66%), Space: 46.8 MB (68.90%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -1,16 +1,28 @@
 class KthLargest {
     
-    List<Integer> listElement = new ArrayList<>();
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
     int kValue = 0;
     public KthLargest(int k, int[] nums) {
         kValue = k;
-        for (int i = 0; i < nums.length; i++) listElement.add(nums[i]);
+        for (int i = 0; i < nums.length; i++) {
+            if (minHeap.size() == k) {
+                if (nums[i] > minHeap.peek()) {
+                    minHeap.poll();
+                    minHeap.add(nums[i]);
+                }
+            }
+            else minHeap.add(nums[i]);
+        }
     }
     
     public int add(int val) {
-        listElement.add(val);
-        Collections.sort(listElement, Collections.reverseOrder());
-        return listElement.get(kValue - 1);
+        if (minHeap.size() >= kValue) {
+            if (val > minHeap.peek()) {
+                minHeap.poll();
+                minHeap.add(val);
+            }
+        } else minHeap.add(val);
+        return minHeap.peek();
     }
 }
 
@@ -19,3 +31,20 @@ class KthLargest {
  * KthLargest obj = new KthLargest(k, nums);
  * int param_1 = obj.add(val);
  */
+
+// Brute Force Approach.
+// class KthLargest {
+    
+//     List<Integer> listElement = new ArrayList<>();
+//     int kValue = 0;
+//     public KthLargest(int k, int[] nums) {
+//         kValue = k;
+//         for (int i = 0; i < nums.length; i++) listElement.add(nums[i]);
+//     }
+    
+//     public int add(int val) {
+//         listElement.add(val);
+//         Collections.sort(listElement, Collections.reverseOrder());
+//         return listElement.get(kValue - 1);
+//     }
+// }
