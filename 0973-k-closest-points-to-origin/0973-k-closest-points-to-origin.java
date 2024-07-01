44a1d78480f7e00801cfda287bb3f7b62ec6a5a4
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-07-01 19:33:05 +0530

    Time: 26 ms (69.75%), Space: 52.8 MB (50.75%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,29 +1,24 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        int n = points.length;
-        Arrays.sort(points, (point1, point2) -> {
-            int num1 = calcDistance(point1);
-            int num2 = calcDistance(point2);
-            if (num1 < num2) {
-                return -1; 
-            } 
-            else if (num1 > num2) {
-                return 1;
-            }
-            else {
-                return 0;
-            }
+        Arrays.sort(points, (e1, e2)->{
+            int distance1 = distance(e1[0], e1[1]);
+            int distance2 = distance(e2[0], e2[1]);
+            return distance1 - distance2;
         });
-        int[][] answerArray = new int[k][2];
-        int index = 0;
+        int[][] answer = new int[k][2];
         for (int i = 0; i < k; i++) {
-            answerArray[index++] = points[i];
+            answer[i][0] = points[i][0];
+            answer[i][1] = points[i][1];
         }
-        return answerArray;
+        return answer;
     }
-    private int calcDistance(int[] point) {
-        long num1 = (point[1] * 1l) * point[1];
-        long num2 = (point[0] * 1l) * point[0];
-        return (int) (num1 + num2);
+    private int distance(int x1, int y1) {
+        int num1 = x1 * x1;
+        int num2 = y1 * y1;
+        return num1 + num2;
     }
 }
+
+
+// The distance between two points on the X-Y plane is the Euclidean distance 
+// Math.sqrt((x1 - x2)^2 + (y1 - y2)^2)
