8d62b2a0cb7f29eb08ea5e8b098dedbb44f88f47
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-07-08 02:16:17 +0530

    Time: 2 ms (85.53%), Space: 44.5 MB (40.19%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,5 +1,6 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
         List<List<Integer>> listAnswer = new ArrayList<>();
         find(listAnswer, new ArrayList<>(), candidates, target, 0);
         return listAnswer;
@@ -9,14 +10,13 @@ class Solution {
             listAnswer.add(new ArrayList<>(listSubAnswer));
             return;
         }
-        else if (index >= candidates.length) {
+        else if (index >= candidates.length || candidates[index] > target) {
             return;
         }
         
         // include element.
         listSubAnswer.add(candidates[index]);
-        if (candidates[index] <= target)
-            find(listAnswer, listSubAnswer, candidates, target - candidates[index], index);
+        find(listAnswer, listSubAnswer, candidates, target - candidates[index], index);
         
         // exclude element.
         listSubAnswer.remove(listSubAnswer.size() - 1);
