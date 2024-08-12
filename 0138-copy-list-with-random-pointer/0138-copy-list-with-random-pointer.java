24bdc007c3333bc827b8c593e720a24ca436e153
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-08-13 01:26:51 +0530

    Time: 0 ms (100.00%), Space: 44.7 MB (18.81%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -17,38 +17,35 @@ class Solution {
     public Node copyRandomList(Node head) {
         if (head == null) return head;
         
-        // Step 1: Insert a new node after every node in the given linked list.
+        // Step1: Insert new node after every node.
         Node current = head;
-        Node future = null;
         while (current != null) {
-            Node newNode = new Node(current.val);
-            future = current.next;
-            current.next = newNode;
-            newNode.next = future;
+            Node future = current.next;
+            current.next = new Node(current.val);
+            current.next.next = future;
             current = future;
-        } 
+        }
         
-        // Step 2: Point the random pointers of the newly inserted nodes correctly.
+        // Step2: Point random pointers correctly.
         current = head;
         while (current != null) {
-            future = current.next;
+            Node future = current.next;
             if (current.random != null)
                 current.next.random = current.random.next;
             current = future.next;
         }
         
-        // Step 3: Decouple the linked list in such a way that part 1 contains the given input linked list and part 2 contains the newly created nodes linked list.
+        // Decouple the linked list to take the copied linked list.
+        Node answerHead = head.next;
         current = head;
-        Node answerNode = head.next;
         while (current != null) {
-            future = current.next;
+            Node future = current.next;
             current.next = future.next;
-            if (future.next != null)
-                future.next = future.next.next;
+            if (current.next != null)
+                future.next = current.next.next;
             current = current.next;
         }
         
-        // Step 4: Return Part 2 as the copied random list.
-        return answerNode;
+        return answerHead;
     }
 }
