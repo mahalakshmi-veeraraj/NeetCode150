07e1f8afe4f1abc45018f3ec0da8c42f6095a66f
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-08-12 13:52:19 +0530

    Time: 3 ms (100.00%), Space: 42 MB (34.07%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,41 +1,32 @@
 class Solution {
     public int search(int[] nums, int target) {
         int minElementIndex = findMin(nums);
+        System.out.println(minElementIndex);
         int answer = binarySearch(nums, 0, minElementIndex - 1, target);
-        if (answer != -1) {
-            return answer;
+        if (answer == -1) {
+            answer = binarySearch(nums, minElementIndex, nums.length - 1, target);
         }
-        return binarySearch(nums, minElementIndex, nums.length - 1, target);
+        return answer;
     }
-    private int findMin(int[] nums) {
+    private int findMin (int[] nums) {
         int low = 0;
         int high = nums.length - 1;
         int middle = 0;
         while (low < high) {
             middle = (low + high) / 2;
-            if (nums[middle] <= nums[high]) {
-                high = middle;
-            }
-            else {
-                low = middle + 1;
-            }
+            if (nums[middle] <= nums[high]) high = middle;
+            else low = middle + 1;
         }
         return low;
     }
-    private int binarySearch(int[] nums, int low, int high, int target) {
+    private int binarySearch (int[] nums, int low, int high, int target) {
         int middle = 0;
         while (low <= high) {
             middle = (low + high) / 2;
-            if (nums[middle] == target) {
-                return middle;
-            }
-            else if (nums[middle] < target) {
-                low = middle + 1;
-            }
-            else {
-                high = middle - 1;
-            }
-        }
+            if (nums[middle] == target) return middle;
+            else if (nums[middle] > target) high = middle - 1;
+            else low = middle + 1;
+        } 
         return -1;
     }
 }
