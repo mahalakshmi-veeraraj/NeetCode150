1440c3d7deb575a3db727248489157216c8bc57b
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-08-13 08:55:41 +0530

    Time: 52 ms (30.10%), Space: 122.4 MB (24.53%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,25 +1,23 @@
 class LRUCache {
     class Node {
-        Node next;
-        Node previous;
         int key;
         int value;
+        Node next;
+        Node previous;
         public Node(int key, int value) {
             this.key = key;
             this.value = value;
         }
     }
-    
-    Map<Integer, Node> hashMap = new HashMap<>();
     Node head = new Node(-1, -1);
     Node tail = new Node(-1, -1);
-    int actualCapacity;
+    Map<Integer, Node> hashMap = new HashMap<>();
+    int actualCapacity = 0;
     int currentCapacity = 0;
-    
     public LRUCache(int capacity) {
-        this.actualCapacity = capacity;
         head.next = tail;
         tail.previous = head;
+        actualCapacity = capacity;
     }
     
     public int get(int key) {
@@ -27,50 +25,50 @@ class LRUCache {
             return -1;
         }
         Node node = hashMap.get(key);
-        int value = node.value;
         deleteNode(node);
+        hashMap.remove(key);
         addNode(node);
         hashMap.put(key, tail.previous);
-        return value;
+        return node.value;
     }
     
     public void put(int key, int value) {
         if (hashMap.containsKey(key)) {
             Node node = hashMap.get(key);
             deleteNode(node);
+            hashMap.remove(key);
             node.value = value;
             addNode(node);
             hashMap.put(key, tail.previous);
         }
         else if (currentCapacity < actualCapacity) {
-            Node addNode = new Node(key, value);
-            addNode(addNode);
+            Node newNode = new Node(key, value);
+            addNode(newNode);
             hashMap.put(key, tail.previous);
             currentCapacity++;
         }
         else {
+            Node newNode = new Node(key, value);
             Node deleteNode = head.next;
             deleteNode(deleteNode);
             hashMap.remove(deleteNode.key);
-            Node addNode = new Node(key, value);
-            addNode(addNode);
+            addNode(newNode);
             hashMap.put(key, tail.previous);
         }
     }
     
-    private void addNode(Node addNode) {
+    private void addNode(Node newNode) {
         Node tailPrevious = tail.previous;
-        tail.previous = addNode;
-        addNode.next = tail;
-        tailPrevious.next = addNode;
-        addNode.previous = tailPrevious;
+        tailPrevious.next = newNode;
+        newNode.previous = tailPrevious;
+        newNode.next = tail;
+        tail.previous = newNode;
     }
     
     private void deleteNode(Node deleteNode) {
         Node deleteNodePrevious = deleteNode.previous;
-        Node deleteNodeNext = deleteNode.next;
-        deleteNodePrevious.next = deleteNodeNext;
-        deleteNodeNext.previous = deleteNodePrevious;
+        deleteNodePrevious.next = deleteNode.next;
+        deleteNode.next.previous = deleteNodePrevious;
     }
 }
 
