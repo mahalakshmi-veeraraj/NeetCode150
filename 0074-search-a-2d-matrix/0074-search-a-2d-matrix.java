6e47d8d81b033946a885c0431b96881e36b98774
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-08-13 15:04:58 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (62.69%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,37 +1,31 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int totalRows = matrix.length;
-        int totalColumns = matrix[0].length;
         int low = 0;
-        int high = totalRows - 1;
+        int high = matrix.length - 1;
         int middle = 0;
-        int answerRowIndex = -1;
+        int searchRow = -1;
         while (low <= high) {
             middle = (low + high) / 2;
-            if (matrix[middle][0] > target) {
-                high = middle - 1;
+            if (matrix[middle][0] <= target) {
+                searchRow = middle;
+                low = middle + 1;
             }
             else {
-                answerRowIndex = middle;
-                low = middle + 1;
+                high = middle - 1;
             }
         }
-        if (answerRowIndex == -1) return false;
+        if (searchRow == -1) return false;
         low = 0;
-        high = totalColumns - 1;
+        high = matrix[0].length - 1;
         while (low <= high) {
             middle = (low + high) / 2;
-            if (matrix[answerRowIndex][middle] == target) {
-                return true;
-            }
-            else if (matrix[answerRowIndex][middle] < target) {
-                low = middle + 1;
-            }
-            else {
-                high = middle - 1;
-            }
+            if (matrix[searchRow][middle] == target) return true;
+            else if (matrix[searchRow][middle] < target) low = middle + 1;
+            else high = middle - 1;
         }
         return false;
     }
 }
 
+
+ 
