1a19d9dd4eec58a3fad890b74b08baf2a722469e
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-08-13 13:54:22 +0530

    Time: 1 ms (39.16%), Space: 45 MB (81.96%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,17 +1,26 @@
 class Solution {
-    public int missingNumber(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == i) continue;
-            while (arr[i] < n && arr[i] != i) {
-                int temp = arr[arr[i]];
-                arr[arr[i]] = arr[i];
-                arr[i] = temp;
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == i + 1) continue;
+            while (nums[i] - 1 >= 0 && nums[i] != i + 1) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
             }
         }
-        for (int i = 0; i < n; i++) {
-            if (arr[i] != i) return i;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == i + 1) continue;
+            return i + 1;
         }
-        return n;
+        return 0;
     }
 }
+
+//3, 0, 1
+//9, 6, 4, 2, 3, 5, 7, 0, 1
+
+//0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+//9, 6, 4, 2, 3, 5, 7, 0, 1
+
+// 0, 1, 2, 3, 4, 5, 6, 7, 9
