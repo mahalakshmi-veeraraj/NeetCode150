f20439e134350737d8ac0f50692904537944ea9d
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-08-31 12:29:20 +0530

    Time: 2 ms (52.27%), Space: 41.1 MB (94.01%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,26 +2,16 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         int n = s.length();
-        char peekChar = '\0';
-        char currentChar = '\0';
         for (int i = 0; i < n; i++) {
-            currentChar = s.charAt(i);
-            if (stack.empty()) {
-                stack.push(currentChar);
-                continue;
-            }
-            peekChar = stack.peek();
-            if (peekChar == '[' && currentChar == ']') {
-                stack.pop();
-            }
-            else if (peekChar == '{' && currentChar == '}') {
-                stack.pop();
-            }
-            else if (peekChar == '(' && currentChar == ')') {
-                stack.pop();
-            }
-            else {
-                stack.push(currentChar);
+            char currChar = s.charAt(i);
+            if (stack.empty() || currChar == '{' || currChar == '[' || currChar == '(') {
+                stack.push(currChar);
+            } 
+            else if (!stack.empty()) {
+                char topChar = stack.pop();
+                if (currChar == '}' && topChar != '{') return false;
+                else if (currChar == ']' && topChar != '[') return false;
+                else if (currChar == ')' && topChar != '(') return false;
             }
         }
         return stack.empty();
