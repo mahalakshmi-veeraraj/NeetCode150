a898b12b41b5bfb58acdd2bf2cbba040dd82ea45
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-10-26 20:47:40 +0530

    Time: 38 ms (68.86%), Space: 44.2 MB (47.01%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,36 +1,21 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[n][n + 1];
-        for (int i = 0; i < dp.length; i++) {
-            for (int j = 0; j < dp[i].length; j++) {
-                dp[i][j] = -1;
+        int[] dp = new int[n];
+        for (int index = 0; index < n; index++) {
+            dp[index] = 1;
+        }
+        for (int index = 1; index < n; index++) {
+            for (int previousIndex = index - 1; previousIndex >= 0; previousIndex--) {
+                if (nums[previousIndex] < nums[index]) {
+                    dp[index] = Math.max(dp[index], dp[previousIndex] + 1);
+                }
             }
         }
-        find (nums, dp, 0, -1);
         int answer = Integer.MIN_VALUE;
-        for (int i = 0; i < dp.length; i++) {
-            for (int j = 0; j < dp[i].length; j++) {
-                answer = Math.max(answer, dp[i][j]);
-            }
+        for (int index = 0; index < n; index++) {
+            answer = Math.max(answer, dp[index]);
         }
         return answer;
-    }
-    private int find (int[] nums, int[][] dp, int index, int previousIndex) {
-        if (index >= nums.length) return 0;
-        
-        if (dp[index][previousIndex + 1] != -1) return dp[index][previousIndex + 1];
-        
-        // Include the element.
-        int include = 0;
-        if (previousIndex == -1 || nums[index] > nums[previousIndex]) 
-            include = find (nums, dp, index + 1, index) + 1;
-        
-        // Exclude the element.
-        int exclude = find (nums, dp, index + 1, previousIndex);
-        
-        dp[index][previousIndex + 1] = Math.max(include, exclude);
-        
-        return dp[index][previousIndex + 1];
     }
 }
