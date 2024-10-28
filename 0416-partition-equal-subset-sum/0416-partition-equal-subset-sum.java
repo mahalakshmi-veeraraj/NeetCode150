62c83d9cb8d293d06836ade7ad50aabe476fd096
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-10-28 20:58:03 +0530

    Time: 115 ms (11.37%), Space: 58.5 MB (10.50%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,32 +1,35 @@
 class Solution {
     public boolean canPartition(int[] nums) {
+        int n = nums.length;
         int sum = 0;
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < n; i++) {
             sum += nums[i];
         }
         if (sum % 2 != 0) return false;
         
         int target = sum / 2;
-        int[][] dp = new int[nums.length][target + 1];
-        for (int row = 0; row < dp.length; row++) {
-            for (int col = 0; col < dp[0].length; col++) {
-                dp[row][col] = -1;
+        int[][] dp = new int[n][target + 1];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[i].length; j++) {
+                dp[i][j] = -1;
             }
         }
-        return find(nums, target, 0, dp) == 1;
+        return find (nums, target, 0, dp);
     }
-    private int find (int[] nums, int target, int index, int[][] dp) {
-        if (target == 0) return 1;
-        if (index >= nums.length) return 0;
-        if (dp[index][target] != -1) return dp[index][target];
-        if (nums[index] > target) {
-            
-            dp[index][target] = find(nums, target, index + 1, dp);
-            return dp[index][target];
-        }
-        int answer1 = find (nums, target - nums[index], index + 1, dp);
-        int answer2 = find(nums, target, index + 1, dp);
-        dp[index][target] = answer1 == 1 || answer2 == 1 ? 1 : 0;
-        return dp[index][target];
+    private boolean find (int[] nums, int target, int index, int[][] dp) {
+        if (target == 0) return true;
+        if (index >= nums.length) return false;
+        
+        if (dp[index][target] != -1) return dp[index][target] == 1;
+        
+        boolean result = false;
+        // include the element.
+        if (nums[index] <= target)
+            result |= find (nums, target - nums[index], index + 1, dp);
+        
+        // exclude the element.
+        result |= find (nums, target, index + 1, dp);
+        dp[index][target] = result ? 1 : 0;
+        return result;
     }
 }
