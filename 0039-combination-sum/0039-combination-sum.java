b54104e41ca13210852738815dd8c38f8e6711d6
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-11-01 19:30:44 +0530

    Time: 2 ms (84.81%), Space: 44.4 MB (67.25%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,22 +2,26 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> listAnswer = new ArrayList<>();
         List<Integer> listSubAnswer = new ArrayList<>();
-        Arrays.sort(candidates);
-        find(listAnswer, listSubAnswer, candidates, target, 0);
+        find (listAnswer, listSubAnswer, 0, target, candidates);
         return listAnswer;
     }
-    private void find (List<List<Integer>> listAnswer, List<Integer> listSubAnswer, int[] candidates, int target, int index) {
+    private void find (List<List<Integer>> listAnswer, List<Integer> listSubAnswer, int index, int target, int[] candidates) {
         if (target == 0) {
             listAnswer.add(new ArrayList<>(listSubAnswer));
             return;
         }
+        if (index >= candidates.length) return;
         
-        if (index >= candidates.length || candidates[index] > target) return;
+        // take element.
+        int take = -1;
+        if (candidates[index] <= target) {
+            listSubAnswer.add(candidates[index]);
+            find (listAnswer, listSubAnswer, index, target - candidates[index], candidates);
+            take = 1;
+        }
         
-        listSubAnswer.add(candidates[index]);
-        find(listAnswer, listSubAnswer, candidates, target - candidates[index], index);
-        
-        listSubAnswer.remove(listSubAnswer.size() - 1);
-        find(listAnswer, listSubAnswer, candidates, target, index + 1);
+        // not take element.
+        if (take != -1) listSubAnswer.remove(listSubAnswer.size() - 1);
+        find (listAnswer, listSubAnswer, index + 1, target, candidates);
     }
 }
