23526ed5683b0c38fb09c827b3f88aa8047355ac
Mahalakshmi Veeraraj <143511203+mahalakshmi-veeraraj@users.noreply.github.com>
2024-11-08 04:52:53 -0700

    Time: 2 ms (40.33%), Space: 45 MB (29.38%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,35 +1,44 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        // listInterval preparing part.
-        List<List<Integer>> listInterval = new ArrayList<>();
-        for (int i = 0; i < intervals.length; i++) {
-            List<Integer> listSubInterval = new ArrayList<>();
-            listSubInterval.add(intervals[i][0]);
-            listSubInterval.add(intervals[i][1]);
-            listInterval.add(listSubInterval);
-        }
-        List<Integer> listSubInterval = new ArrayList<>();
-        listSubInterval.add(newInterval[0]);
-        listSubInterval.add(newInterval[1]);
-        listInterval.add(listSubInterval);
-        Collections.sort(listInterval, (a, b) -> Integer.compare(a.get(0), b.get(0)));
-        
-        // listAnswer preparing part.
+        int n = intervals.length;
         List<List<Integer>> listAnswer = new ArrayList<>();
-        int startInterval = listInterval.get(0).get(0);
-        int endInterval = listInterval.get(0).get(1);
-        for (int i = 0; i < listInterval.size(); i++) {
-            if (listInterval.get(i).get(0) <= endInterval) {
-                startInterval = Math.min(startInterval, listInterval.get(i).get(0));
-                endInterval = Math.max(endInterval, listInterval.get(i).get(1));
+        
+        int breakIndex = -1;
+        for (int i = 0; i < n; i++) {
+            if (intervals[i][1] < newInterval[0]) {
+                List<Integer> listSubAnswer = new ArrayList<>();
+                listSubAnswer.add(intervals[i][0]);
+                listSubAnswer.add(intervals[i][1]);
+                listAnswer.add(listSubAnswer);
+            } 
+            else {
+                breakIndex = i;
+                break;
+            }
+        }
+        
+        if (breakIndex == -1) {
+            List<Integer> listSubAnswer = new ArrayList<>();
+            listSubAnswer.add(newInterval[0]);
+            listSubAnswer.add(newInterval[1]);
+            listAnswer.add(listSubAnswer);
+            return getAnswerArray(listAnswer);
+        }
+        
+        int startInterval = newInterval[0];
+        int endInterval = newInterval[1];
+        for (int i = breakIndex; i < n; i++) {
+            if (intervals[i][0] <= endInterval) {
+                startInterval = Math.min(startInterval, intervals[i][0]);
+                endInterval = Math.max(endInterval, intervals[i][1]);
             }
             else {
                 List<Integer> listSubAnswer = new ArrayList<>();
                 listSubAnswer.add(startInterval);
                 listSubAnswer.add(endInterval);
                 listAnswer.add(listSubAnswer);
-                startInterval = listInterval.get(i).get(0);
-                endInterval = listInterval.get(i).get(1);
+                startInterval = intervals[i][0];
+                endInterval = intervals[i][1];
             }
         }
         List<Integer> listSubAnswer = new ArrayList<>();
@@ -37,7 +46,9 @@ class Solution {
         listSubAnswer.add(endInterval);
         listAnswer.add(listSubAnswer);
         
-        // answerArray preparing part.
+        return getAnswerArray(listAnswer);
+    }
+    private int[][] getAnswerArray (List<List<Integer>> listAnswer) {
         int[][] answerArray = new int[listAnswer.size()][2];
         for (int i = 0; i < listAnswer.size(); i++) {
             answerArray[i][0] = listAnswer.get(i).get(0);
